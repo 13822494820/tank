f7a95c92af164c92f579474356b44d7abf3b48dd
alter <a123dsa1@qq.com>
2020-02-22 16:35:34 +0800

    去掉敌人，为网络版做准备

--- src/com/mashibing/tank/Main.java
+++ src/com/mashibing/tank/Main.java
@@ -13,12 +13,12 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		TankFrame tf = new TankFrame();
 		
-		int initTankCount = Integer.parseInt((String)PropertyMgr.get("initTankCount"));
-		
-		//初始化敌方tank
-		for(int i=0;i<initTankCount;i++) {
-			tf.tanks.add(new Tank(50 + i*80, 200, Dir.DOWN,Group.BAD, tf));
-		}
+//		int initTankCount = Integer.parseInt((String)PropertyMgr.get("initTankCount"));
+//		
+//		//初始化敌方tank
+//		for(int i=0;i<initTankCount;i++) {
+//			tf.tanks.add(new Tank(50 + i*80, 200, Dir.DOWN,Group.BAD, tf));
+//		}
 		
 		new Thread(()->new Audio("audio/war1.wav").loop()).start();
 		
